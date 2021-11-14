0650cc38bce0f0772d4fae86a28f799816e99aff
Iqbal Hanif <iqbal.hanif.ipb@gmail.com>
2021-11-14 20:44:55 +0700

    Add files via upload

---
+++ dags/blankspace_etl.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1, 
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    dag_id = 'blankspace_etl',
+    start_date = datetime(2020,9,11),
+    schedule_interval = '0 0 * * *',
+    default_args=default_args
+)
+
+# ETL with bash 
+
+bash_etl1 = BashOperator(
+    task_id='beers_etl',
+    bash_command='python3 /home/iqbal_hanif_ipb/gcp/docker-local/pipeline/batch.py --runner DataFlowRunner --project amiable-crane-330914 --temp_location gs://blankspace89783/batch/temp --staging_location gs://blankspace89783/batch/stag --region us-central1 --job_name drinkbeer',
+    dag=dag,
+)
+
+bash_etl2 = BashOperator(
+    task_id='citizens_etl',
+    bash_command='python3 /home/iqbal_hanif_ipb/gcp/docker-local/pipeline/batch2.py --runner DataFlowRunner --project amiable-crane-330914 --temp_location gs://blankspace89783/batch/temp --staging_location gs://blankspace89783/batch/stag --region us-central1 --job_name citizensregister',
+    dag=dag,
+)
+#DAG order
+
+bash_etl1 >> bash_etl2
