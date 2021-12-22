1a44ae6623a7ab3d80b6a34bdcaca8a1b4418821
Iqbal Hanif <iqbal.hanif.ipb@gmail.com>
2021-12-22 15:35:30 +0700

    Add files via upload

---
+++ pipeline/batch-flight.py
@@ -0,0 +1,35 @@
+import apache_beam as beam
+import argparse
+from apache_beam.options.pipeline_options import PipelineOptions
+from sys import argv
+from apache_beam.dataframe import convert
+import os
+
+PROJECT_ID='amiable-crane-330914'
+
+def run(argv=None):
+  parser = argparse.ArgumentParser()
+
+  known_args, pipeline_args = parser.parse_known_args(argv)
+
+  with beam.Pipeline(options=PipelineOptions()) as pipeline:
+    extract_data = (
+        pipeline 
+        | 'Read CSV with beam.dataframe' >> beam.dataframe.io.read_csv('gs://blankspace89783/flight/*.csv') 
+    )
+    
+    extract_data.fillna('',inplace=True)
+    
+    transform_data = (
+        convert.to_pcollection(extract_data)
+        | 'Convert to dictionaries' >> beam.Map(lambda x: dict(x._asdict()))
+    )
+    load_data = (
+        transform_data
+        | "Write data to BigQuery" >> beam.io.WriteToBigQuery(
+           table= '{0}:flight.all_flight'.format(PROJECT_ID),
+           schema='SCHEMA_AUTODETECT',
+           write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)
+    )
+if __name__ == '__main__':
+    run()
